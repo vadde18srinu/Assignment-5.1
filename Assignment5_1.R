4a8c8f87f3b1b0881ae9797df926a6d67367c7d5
vadde18srinu <40682358+vadde18srinu@users.noreply.github.com>
2018-08-10 18:28:12 +0530

    Add files via upload

--- Assignment5_1.R
+++ Assignment5_1.R
@@ -1,17 +1,34 @@
 1. How many vowels are there in the names of USA States?
-library(phonR)
-x="in USA"
-x
 
-vowels=c(x,vowels="U","A") 
+library(dplyr)
+library(states)
+states=row.names(USArrests)
+states
+
+library(stringr)
+
+#vector of vowels 
+vowels=(c("a","e","i","o","u"))
 vowels
+
+num_vowels=vector(mode = "integer",length=5)
+
+for (j in seq_along(vowels)) {
+    num_aux = str_count(tolower(states), vowels[j])
+  num_vowels[j] = sum(num_aux)}
+
+#add vowel names
+names(num_vowels)=vowels
+
+# total number of vowels
+num_vowels
 
 
 2. Visualize the vowels distribution.
 
-library(phonR)
-data(indoVowels)
-plotVowels(indo$f1, indo$f2)
-plotVowels
-summary(indo$f1, indo$f2)
+# Ans: barplot
+
+barplot(num_vowels, main = "Number of vowels in USA States Name",
+        border = NA, ylim = c(0,80))
+
 
